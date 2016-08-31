5441c0a070774242ee2be18729def51dad9abad9
Felipe Romero <feliperomero3@outlook.com>
2016-08-31 15:58:12 -0600

    Add MainDock and remove unnecessary files.

---
+++ Example1/MainDock.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace XceedAvalonDockSandbox
+{
+    /// <summary>
+    /// Interaction logic for MainDock.xaml
+    /// </summary>
+    public partial class MainDock : Window
+    {
+        public MainDock()
+        {
+            InitializeComponent();
+        }
+    }
+}
